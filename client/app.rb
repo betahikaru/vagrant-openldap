65ce7fdc39d18454c57882293e05c188807368cd
betahikaru <beta.hikaru2000+github@gmail.com>
2016-02-12 23:36:56 +0900

    Refactoring

--- client/app.rb
+++ client/app.rb
@@ -24,27 +24,38 @@ def serialize_ldap_entry(entry)
   return output
 end
 
+def json_list_wrapping(list)
+  output = "{\"list\":["
+  list.each do |str|
+    output += str + ","
+  end
+  output.chop!
+  output += "]}"
+  return output
+end
+
+LDAP_HOST = "192.168.33.20"
+LDAP_BASE_DN = "dc=betahikaru,dc=com"
+LDAP_MANAGER_DN = "cn=Manager,dc=betahikaru,dc=com"
+LDAP_MANAGER_PASS = "passwd"
 ldap = Net::LDAP.new(
-  host: "192.168.33.20",
+  host: LDAP_HOST,
   port: 389,
   auth: {
     method: :simple,
-    username: "cn=Manager,dc=betahikaru,dc=com",
-    password: "passwd"
+    username: LDAP_MANAGER_DN,
+    password: LDAP_MANAGER_PASS
   }
 )
 
 begin
   if ldap.bind
     filter = Net::LDAP::Filter.eq("cn", "* Suzuki")
-    tree_base = "dc=betahikaru,dc=com"
-    output = "{\"list\":["
-    ldap.search(base: tree_base, filter: filter) do |entry|
-      output += serialize_ldap_entry(entry)
-      output += ","
+    serilized_entries = []
+    ldap.search(base: LDAP_BASE_DN, filter: filter) do |entry|
+      serilized_entries.push(serialize_ldap_entry(entry))
     end
-    output.chop!
-    output += "]}"
+    output = json_list_wrapping(serilized_entries)
   else
     output += "{ \"error\": \"authentication failed\" }"
     return
